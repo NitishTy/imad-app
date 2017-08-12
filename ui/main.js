cad09d9b387db4c0eb1368e40f4983204922463c
NitishTy <30926526+NitishTy@users.noreply.github.com>
2017-08-13 00:54:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,12 @@ var img= document.getElementById('picture');
 img.onclick= function () {
     img.style.marginLeft='100px';
 };
+
+var marginTop= 0;
+function moveBottom() {
+    marginTop = marginTop + 1;
+    img.style.marginTop = marginTop + 'px';
+}
+img.onclick = function () {
+    var interval = setInterval(moveBottom,50);
+};
