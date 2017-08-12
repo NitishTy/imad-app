ee3d9301030807f6e8be5dd92f625c937fafcab4
NitishTy <30926526+NitishTy@users.noreply.github.com>
2017-08-13 00:51:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var img= document.getelementById('picture');
+var img= document.getElementById('picture');
 img.onclick= function () {
     img.style.marginLeft='100px';
 };
