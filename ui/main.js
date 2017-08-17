ff7c47eb3801432df1c68bfc4d70233bf4206baf
NitishTy <30926526+NitishTy@users.noreply.github.com>
2017-08-17 17:37:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -46,7 +46,7 @@ submit.onclick = function () {
     var nameInput = document.getElementById('name');
     var name = nameInput.value;
     //Make the request
-    request.open('GET','http://nitstyagi0.imad.hasura-app.io/counter',true);
+    request.open('GET','http://nitstyagi0.imad.hasura-app.io/submit-name?name=' + name ,true);
     request.send(null);
     // Capture the list of names and render it as a list
    
